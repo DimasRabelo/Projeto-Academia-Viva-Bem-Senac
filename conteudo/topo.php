d07a293148dd156222ade65ced63e9b78305ede0
sowie <dimas.rabelosouza@gmail.com>
2024-01-28 23:29:35 -0300

    update

--- conteudo/topo.php
+++ conteudo/topo.php
@@ -60,7 +60,7 @@
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h2 class="modal-title fs-5" id="loginModalLabel">Área Restrita</h2>
+                                <h1 class="modal-title fs-5" id="loginModalLabel">Área Restrita</h1>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
--- dbvivabem.sql
+++ dbvivabem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28/01/2024 às 18:59
+-- Tempo de geração: 29/01/2024 às 03:24
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.0.28
 
